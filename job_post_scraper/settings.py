aa33d5b57bedbde30315972965e675549959197b
Kev <101847446+kev-the-python-dev@users.noreply.github.com>
2022-05-09 20:12:38 -0700

    Update settings.py

--- job_post_scraper/settings.py
+++ job_post_scraper/settings.py
@@ -68,8 +68,8 @@ ITEM_PIPELINES = {
 #config = configparser.ConfigParser()
 #config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'settings.cfg'))
 #mlab_uri = config.get('MongoDB', 'mlab_uri')
-MONGODB_SERVER = 'mongodb+srv://kingcobrapy:Z6776xp3@cluster0.ifllx.mongodb.net/test'
-MONGODB_DB = 'job_posts'
+MONGODB_SERVER = 'mongodb+srv://<username>:<password>@<server url>'
+MONGODB_DB = '<db_name>'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
